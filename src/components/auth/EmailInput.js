b02eeced5601f236c961ee836aa9002f48c8b22d
SoobihnKim <ksbsj92@naver.com>
2024-07-11 11:53:57 +0900

    # 렌더링 시 입력창에 포커싱

--- src/components/auth/EmailInput.js
+++ src/components/auth/EmailInput.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 const EmailInput = () => {
+
+    const inputRef = useRef();
+
+    // 렌더링 되자마자 입력창에 포커싱
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []); // 한번만 실행하니 빈 배열
+
     return (
         <>
             <p>Step 1: 유효한 이메일을 입력해주세요.</p>
             <input
+                ref={inputRef}
                 type="email"
                 placeholder="Enter your email"
             />
