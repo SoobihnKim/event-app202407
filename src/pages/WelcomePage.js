8a54038d0a9fa8284738a9e141ff71e5dd81309d
SoobihnKim <ksbsj92@naver.com>
2024-07-16 09:57:23 +0900

    # 로그인 조건부 렌더링

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LoginForm from "../components/auth/LoginForm";
+import Main from "../components/Main";
 
 const WelcomePage = () => {
-    return <LoginForm />;
+
+    console.log('welcomePage 실행');
+
+    // 로그인 여부
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem);
+
+    useEffect(() => {
+        const userData = localStorage.getItem('userData');
+
+        if (userData) setIsLoggedIn(true);
+        else setIsLoggedIn(false);
+    }, [isLoggedIn]);
+
+    return (
+        <>
+            {!isLoggedIn && <LoginForm/>}
+            {isLoggedIn && <Main/>}
+        </>
+    );
 };
 
 export default WelcomePage;
