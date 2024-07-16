e1cb91c43ecefea8e637e3147aebeef9216d4321
SoobihnKim <ksbsj92@naver.com>
2024-07-16 10:43:27 +0900

    # loader 데이터 활용

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {useRouteLoaderData} from "react-router-dom";
 
 const Main = () => {
+
+    // 상위 라우트 페이지의 loader 데이터 불러오기
+    const { email, role } = useRouteLoaderData('user-data');
+    // console.log('userData: ', userData);
+
     return (
         <>
-            <h2>xxx님 환영합니다.</h2>
-            <h3>현재 권한: [VIP]</h3>
+            <h2>{email}님 환영합니다.</h2>
+            <h3>현재 권한: [{ role }]</h3>
             <button>Logout</button>
         </>
     );
--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {NavLink, useLoaderData} from 'react-router-dom';
+import {NavLink, useLoaderData, useRouteLoaderData} from 'react-router-dom';
 import styles from './MainNavigation.module.scss';
 
 const MainNavigation = () => {
 
-    // const data = useLoaderData();
-    // console.log('data: ', data);
+    // 상위 라우트 페이지의 loader 데이터 불러오기
+    const userData = useRouteLoaderData('user-data');
+    console.log('userData: ', userData);
 
-    const activeFn = ({ isActive }) => {
-      // NavLink 컴포넌트에 className 프롭스에 함수를 전달하면
-      // 첫번째 파라미터에 어떤 객체 정보를 준다.
+    const activeFn = ({isActive}) => {
+        // NavLink 컴포넌트에 className 프롭스에 함수를 전달하면
+        // 첫번째 파라미터에 어떤 객체 정보를 준다.
         // console.log(aa);
         return isActive ? styles.active : undefined;
     };
@@ -22,8 +23,15 @@ const MainNavigation = () => {
                         <NavLink to='' className={activeFn} end>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to='events' className={activeFn} >Events</NavLink>
+                        <NavLink to='events' className={activeFn}>Events</NavLink>
                     </li>
+
+                    { userData && (
+                        <li>
+                            <button style={{width: '100%'}}>Logout</button>
+                        </li>
+                    )
+                    }
                 </ul>
             </nav>
         </header>
