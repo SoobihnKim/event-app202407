8f5f6e692d8419ffb329c965f4a0fa804031eec4
SoobihnKim <ksbsj92@naver.com>
2024-07-17 11:40:07 +0900

    # 일반회원 이벤트 생성 제한 처리(useContext, Provider)

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,31 +1,31 @@
-
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter} from 'react-router-dom';
 
 import Home from '../pages/Home';
 import RootLayout from '../layout/RootLayout';
 import ErrorPage from '../pages/ErrorPage';
 import Events from '../pages/Events';
 import EventDetail,
-{ loader as eventDetailLoader, action as deleteAction }
+{loader as eventDetailLoader, action as deleteAction}
     from '../pages/EventDetail';
 import EventLayout from '../layout/EventLayout';
 import NewEvent from '../pages/NewEvent';
 import EditPage from '../pages/EditPage';
-import { action as manipulateAction }
+import {action as manipulateAction}
     from '../components/EventForm';
 import WelcomePage from "../pages/WelcomePage";
 import SignUpPage from "../pages/SignUpPage";
 import {loginAction} from "../components/auth/LoginForm";
 import {authCheckLoader, userDataLoader} from "./auth";
 import {logoutAction} from "../pages/Logout";
+import EventProvider from "../components/context/EventProvider";
 
 
 // 라우터 설정
 const eventsRouter = [
     {
         index: true,
-        element: <Events />,
+        element: <Events/>,
         // loader: eventListLoader, 최초 한번만 실행하는데 무한스크롤로 계속 호출해야해서 안씀
 
     },
@@ -39,19 +39,19 @@ const eventsRouter = [
         children: [
             {
                 index: true,
-                element: <EventDetail />,
+                element: <EventDetail/>,
                 action: deleteAction
             },
             {
                 path: 'edit',
-                element: <EditPage />,
+                element: <EditPage/>,
                 action: manipulateAction
             },
         ]
     },
     {
         path: 'new',
-        element: <NewEvent />,
+        element: <NewEvent/>,
         // 서버에 갱신데이터요청을 보낼 때 트리거
         action: manipulateAction
     },
@@ -61,12 +61,12 @@ const homeRouter = [
 
     {
         index: true,
-        element: <WelcomePage />,
+        element: <WelcomePage/>,
         action: loginAction
     }, // 웰컴 페이지(로그인 화면 or 로그인 완료 화면)
     {
         path: 'sign-up',
-        element: <SignUpPage />
+        element: <SignUpPage/>
     }, // 회원가입 페이지
     {
         path: 'logout',
@@ -78,19 +78,22 @@ const homeRouter = [
 export const router = createBrowserRouter([
     {
         path: '/', // 최상단에 loader
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
         loader: userDataLoader,
         id: 'user-data', // id를 통해 <Home />, homeRouter 쓸 수 있음
         children: [
             {
                 path: '/',
-                element: <Home />,
+                element: <Home/>,
                 children: homeRouter
             },
             {
                 path: 'events',
-                element: <EventLayout />,
+                element: (
+                    <EventProvider>
+                        <EventLayout/>
+                    </EventProvider>),
                 loader: authCheckLoader,
                 children: eventsRouter
             },
--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import EventForm from "../components/EventForm";
-import {redirect} from "react-router-dom";
+import {redirect, useNavigate, useRouteLoaderData} from "react-router-dom";
+import EventContext from "../components/context/event-context";
 
 const NewEvent = () => {
+
+    const {role} = useRouteLoaderData('user-data');
+
+    const navigate = useNavigate();
+
+    const {totalEventCount} = useContext(EventContext);
+
+    // 라우트 가드 (권한, 개수 검증)
+    useEffect(() => {
+        if(role === 'COMMON' && totalEventCount >= 4) {
+            alert('일반 회원은 이벤트 생성이 4개로 제한됩니다.');
+            navigate('/events');
+        }
+    }, [totalEventCount]);
+
     return <EventForm method='post'/>;
 
 };
